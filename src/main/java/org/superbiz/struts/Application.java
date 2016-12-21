527e67ea4308b5b0264ec94edf440ee4559a0103
Damien Le Berrigaud <damien@itsdamo.com>
2016-12-20 17:04:07 -0700

    Cleanup application class.

--- src/main/java/org/superbiz/struts/Application.java
+++ src/main/java/org/superbiz/struts/Application.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 
+import javax.servlet.Filter;
+
 import static java.util.Collections.singletonList;
 
 @SpringBootApplication
@@ -19,28 +21,25 @@ public class Application {
 
     @Bean
     public FilterRegistrationBean filterDispatcher() {
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(new FilterDispatcher());
-        registration.setUrlPatterns(singletonList("/*"));
-        registration.setOrder(0);
-        return registration;
+        return buildFilterRegistration(0, new FilterDispatcher());
     }
 
     @Bean
     public FilterRegistrationBean actionContextCleanup() {
-        FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(new ActionContextCleanUp());
-        registration.setUrlPatterns(singletonList("/*"));
-        registration.setOrder(1);
-        return registration;
+        return buildFilterRegistration(1, new ActionContextCleanUp());
     }
 
     @Bean
     public FilterRegistrationBean sitemeshPageFilter() {
+        return buildFilterRegistration(2, new PageFilter());
+    }
+
+
+    private FilterRegistrationBean buildFilterRegistration(int order, Filter filter) {
         FilterRegistrationBean registration = new FilterRegistrationBean();
-        registration.setFilter(new PageFilter());
+        registration.setFilter(filter);
         registration.setUrlPatterns(singletonList("/*"));
-        registration.setOrder(2);
+        registration.setOrder(order);
         return registration;
     }
 }
