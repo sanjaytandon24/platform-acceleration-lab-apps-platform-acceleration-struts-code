339f99088960231319b67fe274c52744ecc6f2f6
Damien Le Berrigaud <damien@itsdamo.com>
2016-12-30 13:54:19 -0700

    Setup FilterRegistrationBeans

--- src/main/java/org/superbiz/struts/Application.java
+++ src/main/java/org/superbiz/struts/Application.java
@@ -1,12 +1,46 @@
 package org.superbiz.struts;
 
+import com.opensymphony.module.sitemesh.filter.PageFilter;
+import org.apache.struts2.dispatcher.ActionContextCleanUp;
+import org.apache.struts2.dispatcher.FilterDispatcher;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+
+import javax.servlet.Filter;
+
+import static java.util.Collections.singletonList;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String... args) {
         SpringApplication.run(Application.class, args);
+    }
+
+
+    @Bean
+    public FilterRegistrationBean filterDispatcher() {
+        return buildFilterRegistration(0, new FilterDispatcher());
+    }
+
+    @Bean
+    public FilterRegistrationBean actionContextCleanup() {
+        return buildFilterRegistration(1, new ActionContextCleanUp());
+    }
+
+    @Bean
+    public FilterRegistrationBean sitemeshPageFilter() {
+        return buildFilterRegistration(2, new PageFilter());
+    }
+
+
+    private FilterRegistrationBean buildFilterRegistration(int order, Filter filter) {
+        FilterRegistrationBean registration = new FilterRegistrationBean();
+        registration.setFilter(filter);
+        registration.setUrlPatterns(singletonList("/*"));
+        registration.setOrder(order);
+        return registration;
     }
 }
